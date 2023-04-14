7cab8a2a1ade8c3f0f72f4eaba6a6188672765b4
DaveJacobs <116979202+DJacobs86@users.noreply.github.com>
2023-04-13 20:22:20 -0500

    Add files via upload

---
+++ Resources/Queries.sql
@@ -0,0 +1,28 @@
+
+
+
+CREATE TABLE "NPCS" (
+    "name" varchar    NULL,
+    "parkCode" varchar    NULL,
+    "state" varchar    NULL,
+    "longitude" float    NULL,
+    "latitude" float    NULL,
+    "totalSites" int    NULL,
+    "tentOnly" int    NULL,
+    "electricalHookups" int    NULL,
+    "showers" varchar   NULL,
+    "dumpStation" varchar    NULL,
+    "host" varchar    NULL,
+    "potableWater" varchar    NULL,
+    "firewoodForSale" varchar    NULL,
+    "reservationURL" varchar(200)   NULL
+);
+
+CREATE TABLE "VBSBP" (
+    "state" varchar    NULL,
+    "park" varchar    NULL,
+    "RecreationVisitors" INTEGER    NULL,
+    "PercentChange" FLOAT    NULL
+);
+
+
